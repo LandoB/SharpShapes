2705bc355feea67fdcf1f045eda704f0fbad69ec
Lando B <jrolandobernal@gmail.com>
2015-08-22 09:51:57 -0500

    Rectangle.cs and RectangleTest.cs completed. Test passed.

---
+++ UnitTestShapes/RectangleTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes;
+
+namespace UnitTestShapes
+{
+    [TestClass]
+    public class RectangleTest
+    {
+        [TestMethod]
+        public void RectangleAreaTest()
+        {
+            var testRectangle = new Rectangles();
+            testRectangle.Side1 = 4;
+            testRectangle.Side2 = 6;
+
+            Assert.AreEqual(testRectangle.Area(), 24);
+        }
+
+        [TestMethod]
+        public void RectanglePerimeterTest()
+        {
+            var testRectangle = new Rectangles();
+            testRectangle.Side1 = 4;
+            testRectangle.Side2 = 6;
+
+            Assert.AreEqual(testRectangle.Perimeter(), 20);
+        }
+    }
+}
